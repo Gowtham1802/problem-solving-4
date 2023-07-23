512eb9ed6b50a62c562526fbbefca5faf1bc902f
Gowtham1802 <gowthama7575@gmail.com>
2023-07-23 13:24:47 +0530

    add

---
+++ script.js
@@ -0,0 +1,88 @@
+
+let name1 = "Gowtham";
+let name2 = "❤️💙";
+let sum = name1+" "+name2;
+console.log(sum);
+
+
+// 1.Write a JavaScript function that counts the number of vowels in each string.
+
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vol_count = 0;
+  
+  for(var i = 0; i < str1.length ; i++)
+  {
+    if (vowel_list.indexOf(str1[i]) !== -1)
+    {
+      vol_count += 1;
+    }
+  
+  }
+  return vol_count;
+}
+console.log(vowel_count("Three are Best Friends here Abi, Bala , Gowtham "));
+
+
+
+// 2.Create a program that prints the first 10 Fibonacci numbers.
+
+function fibonacci(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 0;
+    for (i = 0; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
+}
+ 
+console.log("Fibonacci(5): " + fibonacci(5));
+console.log("Fibonacci(8): " + fibonacci(8));
+
+
+// 3.Create a program that checks if a given number is a power of two.
+
+
+function isPowerOfTwo(n) {
+  if (n == 0)
+    return false;
+  
+    return parseInt((Math.ceil((Math.log(n) / Math.log(2)))))
+    == parseInt((Math.floor(((Math.log(n) / Math.log(2))))));
+    }
+  
+    // Driver Code
+  
+    if (isPowerOfTwo(31))
+        console.log("Yes");
+    else
+        console.log("No");
+  
+    if (isPowerOfTwo(64))
+        console.log("Yes");
+    else
+        console.log("No");
+
+
+// 4.Create a program that sorts an array of numbers in ascending order.
+
+
+// Declare and initialize original array
+let marks = [12, 25, 31, 23, 75, 81, 100];
+ 
+// Print Before Sorting Array
+console.log("Original Array");
+console.log(marks);
+ 
+// Call sort function
+marks.sort();
+ 
+console.log("After Sorting in Ascending Order");
+ 
+// Print Sorted Numeric Array
+console.log(marks);
